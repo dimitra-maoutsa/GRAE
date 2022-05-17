0fa67fbdce8ba7a8c5d84e04407cd95c428c2106
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2022-05-17 02:49:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='grae',
       version='0.1',
-      python_requires='>=3.8',
+      python_requires='>=3.7',
       description='main package',
       packages=find_packages(),
       install_requires=[
